c3fdaa9e32bf7b2a8618040b2a69ea7b4909366b
NaziaShaik16 <naziashaik888@gmail.com>
2025-10-17 19:28:12 +0530

    this is commit from Asian Timezone guy

--- src/Demo1/day1.java
+++ src/Demo1/day1.java
@@ -5,6 +5,8 @@ public class day1 {
 	public static void main(String[] args) {
 		
 		System.out.println("This is Nazia Shaik..!");
+		System.out.println("This is Farheena Shaik..!");
+		System.out.println("This is Sameena Shaik..!");
 
 	}
 
